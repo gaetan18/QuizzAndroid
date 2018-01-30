453b4779f3cf3bf1a48d442833cff6f9aba0d3f8
gilian dupuis <gilian.dupuis@orange.fr>
2018-01-30 23:56:57 +0100

    fragment

--- ApplicationQuizz/app/src/main/java/com/example/gaetan/applicationquizz/ScoreActivity.java
+++ ApplicationQuizz/app/src/main/java/com/example/gaetan/applicationquizz/ScoreActivity.java
@@ -5,9 +5,37 @@ import android.os.Bundle;
 
 public class ScoreActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
 }
